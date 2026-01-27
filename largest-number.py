19eb2dcd69a5eaf6201cbba594a3fa084b7bb222
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-01-27 19:58:59 +0530

    largest number code

---
+++ largest-number.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number : "))
+b=int(input("enter secound number : "))
+
+if a>b:
+    print(f"{a} is largest number")
+elif b>a:
+    print(f"{b} is largest number")
+else:
+    print("Both numbers are equal")
