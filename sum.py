c043f64d8aec2f743b07ae550f86439d9050913d
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-07-06 12:27:26 +0530

    sum of number 12345=15

--- sum.py
+++ sum.py
@@ -2,5 +2,5 @@ n = 12345
 sum = 0
 while n > 0:
      sum += n % 10 # extract last digit 
-     n //= 10 # remove last digit,integer division
+     n //= 10  # remove last digit,integer division
      print(sum)
