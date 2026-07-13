04eede3ed645b42a423bb2359719ca07c24f73c9
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-07-13 09:09:57 +0530

    largest number in two numbers

---
+++ largest.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+if a>b:
+    print("The largest number is:", a)
+elif b>a:
+    print("The largest number is:", b)
+else:
+    print("Both numbers are equal.")
